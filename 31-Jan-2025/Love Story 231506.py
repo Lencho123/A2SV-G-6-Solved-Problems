9b4056d08aae3a04d0dbed79320501bb896594be
Lencho123 <152869854+Lencho123@users.noreply.github.com>
2025-01-31 18:47:15 +0300

    Created 'Love Story' by A2SV Hub

---
+++ 31-Jan-2025/Love Story 231506.py
@@ -0,0 +1,14 @@
+# Problem: Love Story - https://codeforces.com/contest/1829/problem/A
+
+t=int(input())
+
+for test in range(t):
+    word='codeforces'
+    s=input()
+    count=0
+    
+    for i in range(10):
+        if word[i] != s[i]:
+            count+=1
+    
+    print(count)
