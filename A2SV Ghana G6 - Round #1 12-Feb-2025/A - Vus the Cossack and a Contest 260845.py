c867e68a6611e703896b4d41a106949a91fbe324
Lencho123 <152869854+Lencho123@users.noreply.github.com>
2025-02-12 18:44:39 +0300

    Created 'A - Vus the Cossack and a Contest' by A2SV Hub

---
+++ A2SV Ghana G6 - Round #1 12-Feb-2025/A - Vus the Cossack and a Contest 260845.py
@@ -0,0 +1,8 @@
+# Problem: A - Vus the Cossack and a Contest - https://codeforces.com/gym/585132/problem/A
+
+n,m,k = [int(num) for num in input().split()]
+
+if n<=m and n<=k:
+    print("Yes")
+else:
+    print("No")
