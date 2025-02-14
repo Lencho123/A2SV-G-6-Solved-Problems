6661625168dd8abefde2ef1d51ae365fdd210348
Lencho123 <152869854+Lencho123@users.noreply.github.com>
2025-02-14 18:56:31 +0300

    Created 'Books' by A2SV Hub

---
+++ 14-Feb-2025/Books 266268.py
@@ -0,0 +1,17 @@
+# Problem: Books - https://codeforces.com/contest/279/problem/B
+
+n,t = [int(i) for i in input().split()]
+a=[int(i) for i in input().split()]
+
+time=0
+left=0
+res=0
+
+for right in range(n):
+    time+=a[right]
+    while time > t:
+        time-=a[left]
+        left+=1
+    
+    res= max(res, right-left+1)
+print(res)
