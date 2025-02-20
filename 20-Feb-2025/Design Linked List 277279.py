e764e7cd1fea47b706540df66a664dbcc8952b25
Lencho123 <152869854+Lencho123@users.noreply.github.com>
2025-02-20 21:22:49 +0300

    Created 'Design Linked List' by A2SV Hub

---
+++ 20-Feb-2025/Design Linked List 277279.py
@@ -0,0 +1,65 @@
+# Problem: Design Linked List - https://leetcode.com/problems/design-linked-list/
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+class MyLinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.length = 0
+
+    def get(self, index: int) -> int:
+        if index < 0 or index >= self.length:
+            return -1
+        cur = self.head
+        for _ in range(index):
+            cur = cur.next
+        return cur.val
+
+    def addAtHead(self, val: int) -> None:
+        new_node = Node(val)
+        new_node.next = self.head
+        self.head = new_node
+        self.length += 1
+
+    def addAtTail(self, val: int) -> None:
+        new_node = Node(val)
+        if not self.head:
+            self.head = new_node
+        else:
+            cur = self.head
+            while cur.next:
+                cur = cur.next
+            cur.next = new_node
+        self.length += 1
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        if index < 0 or index > self.length:
+            return
+        if index == 0:
+            self.addAtHead(val)
+        elif index == self.length:
+            self.addAtTail(val)
+        else:
+            new_node = Node(val)
+            cur = self.head
+            for _ in range(index - 1):
+                cur = cur.next
+            new_node.next = cur.next
+            cur.next = new_node
+            self.length += 1
+
+    def deleteAtIndex(self, index: int) -> None:
+        if index < 0 or index >= self.length:
+            return
+        if index == 0:
+            self.head = self.head.next
+        else:
+            cur = self.head
+            for _ in range(index - 1):
+                cur = cur.next
+            cur.next = cur.next.next
+        self.length -= 1
