cd12a05438393fdf4042de5fc0b74514a30ec774
Lencho123 <152869854+Lencho123@users.noreply.github.com>
2025-04-15 13:16:27 +0300

    Created 'Surrounded Regions' by A2SV Hub

---
+++ 15-Apr-2025/Surrounded Regions 334272.py
@@ -0,0 +1,33 @@
+# Problem: Surrounded Regions - https://leetcode.com/problems/surrounded-regions/
+
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        row = len(board)
+        col = len(board[0])
+        directions = [[1,0],[-1,0],[0,1],[0,-1]]
+        def inbound(r,c):
+            return 0 <= r < row and 0 <= c < col
+
+        def markWith(r,c):
+            board[r][c] = 'U'
+            for dr,dc in directions:
+                _r = dr + r
+                _c = dc + c
+                if inbound(_r,_c) and board[_r][_c] == 'O':
+                    markWith(_r,_c)
+        
+
+        for i in range(row):
+            for j in range(col):
+                if (i == 0 or j == 0 or i == row-1 or j == col - 1) and board[i][j] == 'O':
+                    markWith(i,j)
+
+        for i in range(row):
+            for j in range(col):
+                if board[i][j] == 'U':
+                    board[i][j] = 'O'
+                else:
+                    board[i][j] = 'X'
