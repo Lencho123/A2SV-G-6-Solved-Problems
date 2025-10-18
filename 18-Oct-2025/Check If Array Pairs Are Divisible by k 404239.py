7ab81affe649aa5ddec2860edb15b72eb1cd6f10
Lencho123 <lencholachisa11@gmail.com>
2025-10-18 17:30:10 +0300

    Created 'Check If Array Pairs Are Divisible by k' by A2SV Hub

---
+++ 18-Oct-2025/Check If Array Pairs Are Divisible by k 404239.py
@@ -0,0 +1,20 @@
+# Problem: Check If Array Pairs Are Divisible by k - https://leetcode.com/problems/check-if-array-pairs-are-divisible-by-k/
+
+class Solution:
+    def canArrange(self, arr: List[int], k: int) -> bool:
+        remain_ind = defaultdict(list)
+
+        for i,num in enumerate(arr):
+            r = num%k
+            needed = k-r
+            if needed == k:
+                needed = 0
+            if needed in remain_ind and remain_ind[needed]:
+                remain_ind[needed].pop()
+            else:
+                remain_ind[r].append(i)
+
+        for r in remain_ind:
+            if remain_ind[r]:
+                return False
+        return True
