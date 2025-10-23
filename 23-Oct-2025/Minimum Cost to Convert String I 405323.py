a3a7b5bb650412c81164a7712a3190f67dc0c705
Lencho123 <lencholachisa11@gmail.com>
2025-10-23 19:01:04 +0300

    Created 'Minimum Cost to Convert String I' by A2SV Hub

---
+++ 23-Oct-2025/Minimum Cost to Convert String I 405323.py
@@ -0,0 +1,23 @@
+# Problem: Minimum Cost to Convert String I - https://leetcode.com/problems/minimum-cost-to-convert-string-i/description/?envType=problem-list-v2&envId=shortest-path
+
+class Solution:
+    def minimumCost(self, source: str, target: str, original: List[str], changed: List[str], cost: List[int]) -> int:
+        dp = [[float("inf")]*26 for i in range(26)]
+        
+        for i in range(len(original)):
+            r,c=ord(original[i])-ord("a"), ord(changed[i])-ord("a")
+            dp[r][c] = min(dp[r][c],cost[i])
+        
+        for via in range(26):
+            for i in range(26):
+                for j in range(26):
+                    dp[i][j] = min(dp[i][j], dp[i][via] + dp[via][j])
+                    if i == j:
+                        dp[i][j] = 0
+        
+        res = 0
+        for i in range(len(source)):
+            r,c = ord(source[i])-ord("a"), ord(target[i])-ord("a")
+            res+=dp[r][c]
+
+        return res if res<float("inf") else -1
