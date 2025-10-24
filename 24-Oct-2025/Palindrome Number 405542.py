8ede61a09da76a28bf11bf6d2f603a82f5e56ee6
Lencho123 <lencholachisa11@gmail.com>
2025-10-24 18:21:25 +0300

    Created 'Palindrome Number' by A2SV Hub

---
+++ 24-Oct-2025/Palindrome Number 405542.py
@@ -0,0 +1,12 @@
+# Problem: Palindrome Number - https://leetcode.com/problems/palindrome-number/
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        l = []
+        while x:
+            l.append(x%10)
+            x//=10
+        
+        return l == l[::-1]
