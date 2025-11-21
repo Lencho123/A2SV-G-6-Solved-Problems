ac9d0662e00ba4a1c39a523f62022e3dfe1c81f7
Lencho123 <lencholachisa11@gmail.com>
2025-11-21 23:54:20 +0300

    Created 'Number of Ways to Reach a Position After Exactly k Steps' by A2SV Hub

---
+++ 21-Nov-2025/Number of Ways to Reach a Position After Exactly k Steps 409539.py
@@ -0,0 +1,20 @@
+# Problem: Number of Ways to Reach a Position After Exactly k Steps - https://leetcode.com/problems/number-of-ways-to-reach-a-position-after-exactly-k-steps/
+
+class Solution:
+    def numberOfWays(self, startPos: int, endPos: int, k: int) -> int:
+        dp = {}
+        MOD = 10**9 + 7
+        def countWays(pos,left_steps):
+            if left_steps == 0:
+                if pos == endPos:
+                    return 1
+                return 0
+            
+            if (pos,left_steps) in dp:
+                return dp[(pos,left_steps)]
+            
+            left = countWays(pos-1, left_steps-1)%MOD
+            right = countWays(pos+1, left_steps-1)%MOD
+            dp[(pos,left_steps)] = (left + right)%MOD
+            return dp[(pos,left_steps)]
+        return countWays(startPos, k)
